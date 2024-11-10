e075a1070bb4d18088e735798360997a012e851f
Roselin <roselin.t.m@gmail.com>
2024-11-10 01:41:18 -0500

    Create js file and folder

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,4 @@
-const initialTodos = [
-  {
-    id: "7cec7373-681b-49d9-b065-021d61a69d03",
-    name: "Read the sprint's theory",
-    completed: true,
-    date: new Date(),
-  },
-  {
-    id: "a7bfd5ef-37cc-4fa6-89f2-cac098a8aeba",
-    name: "Read project instructions",
-    completed: false,
-    date: new Date(),
-  },
-  {
-    id: "aa486839-63ab-437f-b8a2-29ab217dff4f",
-    name: "Complete project",
-    completed: false,
-    date: new Date(),
-  },
-];
-
-const validationConfig = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  errorClass: "popup__error_visible",
-  inputErrorClass: "popup__input_type_error",
-  inactiveButtonClass: "button_disabled",
-};
+import { initialTodos, validationConfig } from "../utils/constant.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -44,7 +16,7 @@ const closeModal = (modal) => {
 };
 
 // The logic in this function should all be handled in the Todo class.
-const generateTodo = (data) => {
+/*const generateTodo = (data) => {
   const todoElement = todoTemplate.content
     .querySelector(".todo")
     .cloneNode(true);
@@ -78,7 +50,7 @@ const generateTodo = (data) => {
   });
 
   return todoElement;
-};
+};*/
 
 addTodoButton.addEventListener("click", () => {
   openModal(addTodoPopup);
